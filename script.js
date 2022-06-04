f0239ec46c1b0d2e7353943a0340969b94e50fd2
Gulshan Kumar <88340884+gulshan1002@users.noreply.github.com>
2022-06-04 23:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ function makeSound(expression)
     switch (expression)
         {
             case "w":
-                var audio = new Audio("/sounds/tom-2.mp3")
+                var audio = new Audio("sounds/tom-2.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "a":
-                var audio = new Audio("/sounds/tom-1.mp3")
+                var audio = new Audio("sounds/tom-1.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "s":
-                var audio = new Audio("/sounds/tom-3.mp3")
+                var audio = new Audio("sounds/tom-3.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "d":
-                var audio = new Audio("/sounds/tom-4.mp3")
+                var audio = new Audio("sounds/tom-4.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "j":
-                var audio = new Audio("/sounds/snare.mp3")
+                var audio = new Audio("sounds/snare.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "k":
-                var audio = new Audio("/sounds/crash.mp3")
+                var audio = new Audio("sounds/crash.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
             case "l":
-                var audio = new Audio("/sounds/kick-bass.mp3")
+                var audio = new Audio("sounds/kick-bass.mp3")
                 audio.play();
                 this.style.color= "white";
                 break;
